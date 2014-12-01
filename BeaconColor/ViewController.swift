4287d5bb6926c160325159b2ad1198f4da5ee1d7
Joseph Mari <josephmari@Josephs-MacBook-Pro.local>
2014-12-01 17:18:11 -0500

    Initial Commit

---
+++ BeaconColor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeaconColor
+//
+//  Created by Joseph Mari on 12/1/14.
+//  Copyright (c) 2014 Joseph Mari. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
